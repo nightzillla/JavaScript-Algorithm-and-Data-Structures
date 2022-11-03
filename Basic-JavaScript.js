14714d8b5fe990b091e7e6d40d99665708948a26
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-03 17:14:54 -0500

    Explore Differences Between the var and let Keywords

--- Basic-JavaScript.js
+++ Basic-JavaScript.js
@@ -31,3 +31,27 @@ var myFirstName = "Danny"
 var myLastName = "Nguyen"
 
 /** Understanding Uninitialized Variables */
+// Only change code below this line
+var a = 5
+var b = 10
+var c = "I am a"
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+/** Understanding Case Sensitivity in Variables */
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+/** Explore Differences Between the var and let Keywords */
+let catName = "Oliver";
+let catSound = "Meow!";

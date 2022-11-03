a17cf205d8d0cbdf98c53619de42bec90b1cc4de
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-03 15:01:25 -0500

    Declare String Variables

--- Basic-JavaScript.js
+++ Basic-JavaScript.js
@@ -1,4 +1,4 @@
-// Basic JavaScript
+/** Basic JavaScript */
 // Using // will tell JavaScript to ignore the remainder of the text on the current line. This is an in-line comment:
 // EXAMPLE
 // This is a comment
@@ -7,3 +7,27 @@
 /* This is a
 multi-line comment */
 
+/** Declare JavaScript Variables  */
+// In computer science, data is anything that is meaningful to the computer. JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
+// For example, computers distinguish between numbers, such as the number 12, and strings, such as "12", "dog", or "123 cats", which are collections of characters. Computers can perform mathematical operations on a number, but not on a string.
+// Variables allow computers to store and manipulate data in a dynamic fashion. They do this by using a "label" to point to the data rather than using the data itself. Any of the eight data types may be stored in a variable.
+// Variables are similar to the x and y variables you use in mathematics, which means they're a simple name to represent the data we want to refer to. Computer variables differ from mathematical variables in that they can store different values at different times.
+// EXAMPLES
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = a;
+
+/** Initializing Variables with the Assignment Operator */
+// Define a variable a with var and initialize it to a value of 9.
+var a = 9;
+
+/** Declare String Variables */
+// Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+var myFirstName = "Danny"
+var myLastName = "Nguyen"
+
+/** Understanding Uninitialized Variables */

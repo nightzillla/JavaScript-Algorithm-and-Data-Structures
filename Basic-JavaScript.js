230340c3a3539230f6c1174caba0284e203d694d
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-03 14:24:55 -0500

    basic javascript

---
+++ Basic-JavaScript.js
@@ -0,0 +1,9 @@
+// Basic JavaScript
+// Using // will tell JavaScript to ignore the remainder of the text on the current line. This is an in-line comment:
+// EXAMPLE
+// This is a comment
+
+// You can make a multi-line comment beginning with /* and ending with */. This is a multi-line comment:
+/* This is a
+multi-line comment */
+

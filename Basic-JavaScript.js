b2b112c1e33b5363470435ab1d6cb2b9673ad41d
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-05 15:35:16 -0500

    Constructing Strings with Variables

--- Basic-JavaScript.js
+++ Basic-JavaScript.js
@@ -166,3 +166,15 @@ const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 /** Concatenating Strings with Plus Operator */
 // Build myStr from the strings This is the start. and This is the end. using the + operator. Be sure to include a space between the two strings.
 const myStr = "This is the start." + " This is the end."; // Change this line
+
+/** Concatenating Strings with the Plus Equals Operator */
+// Build myStr over several lines by concatenating these two strings: This is the first sentence. and This is the second sentence. using the += operator. Use the += operator similar to how it is shown in the example and be sure to include a space between the two strings. Start by assigning the first string to myStr, then add on the second string.
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+/** Constructing Strings with Variables */
+// Set myName to a string equal to your name and build myStr with myName between the strings My name is and and I am well!
+// Only change code below this line
+const myName = "Danny";
+const myStr = "My name is" + myName + "and I am well!";
